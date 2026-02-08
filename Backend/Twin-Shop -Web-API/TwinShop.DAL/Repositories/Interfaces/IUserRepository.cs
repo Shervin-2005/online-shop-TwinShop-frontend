2521edc98ddc47dd8e16c8d0f55fa6fff7bfa6fd
Shervin <shervinshams40@gmail.com>
2026-02-08 16:07:41 +0330

    deleting the backend from this repostory(for seprate from frontend)

--- Backend/Twin-Shop -Web-API/TwinShop.DAL/Repositories/Interfaces/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Twin_Shop__Web_API.Entities;
-using Twin_Shop__Web_API.Repositories.Interfaces;
-
-public interface IUserRepository : IGenericRepository<User>
-{
-    Task<User?> GetByPhoneAsync(string phone);
-    Task<User?> GetByEmailAsync(string email);
-    Task<bool> PhoneExistsAsync(string phone);
-}
